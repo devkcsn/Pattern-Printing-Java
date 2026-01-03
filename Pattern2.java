8ef9c9ade6c6bab1720c2b9eae3bd783c8815639
Kumar Chandra Sheo Narayan <kumarchandrasheonarayan@outlook.com>
2026-01-03 23:17:20 +0530

    Add files via upload
    grind day 2

---
+++ Pattern2.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Pattern2 {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter no of lines");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
