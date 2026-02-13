81d8688dfe13fd1d2949e79da7341f5589efa1ba
Kumar Chandra Sheo Narayan <kumarchandrasheonarayan@gmail.com>
2026-02-13 22:58:03 +0530

    Uploaded Pattern31

---
+++ Pattern31.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pattern31 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter the desired no of you want to print: ");
+        int rows = sc.nextInt();
+        System.out.println("Here is your desired Pattern: ");
+        int nums = 1;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(nums + " ");
+                nums++;
+            }
+            System.out.println(" ");
+        }
+    }
+}
