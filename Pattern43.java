bf8d149c57f6273bf1b7af1f79fabe42ccb6693b
Kumar Chandra Sheo Narayan <kumarchandrasheonarayan@gmail.com>
2026-02-21 22:40:36 +0530

    Updating JavaLearning

---
+++ Pattern43.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern43 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter the no of rows you want to print: ");
+        int rows = sc.nextInt();
+        System.out.println("Here is your desired pattern: ");
+
+        for (int i = 1; i <= rows; i++) {
+            for (int nos = 1; nos < i; nos++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= rows - i + 1; j++) {
+                System.out.print("* " + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ Pattern44.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern44 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter the no of rows: ");
+        int rows = sc.nextInt();
+        System.out.println("Here is your desired patterm: ");
+
+        for (int i = 1; i <= rows; i++) {
+            for (int nos = 1; nos <= rows - i; nos++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
